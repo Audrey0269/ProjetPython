2a91c72bf8e0ae7fc398d721c3a3f37169a2de05
GAUDILL1 <GAUDILL1@FRLAPP143>
2024-03-12 18:40:22 +0100

    Adding routes and creating html pages

--- script.py
+++ script.py
@@ -1,8 +1,49 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask import url_for
+from markupsafe import escape
+from flask import request
+
 
 app = Flask(__name__)
 
-@app.route("/")
 
-def hello_world():
-    return "<p>Hello!</p>"
+
+#HOME PAGE
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+#RECIPE PAGE
+@app.route('/recipes') #/<int:id>')
+def recipes(): #mettre (id) entre les parenthèses
+    return render_template('recipes.html') #mettre apres : html', id=id)
+
+
+#LOGIN PAGE
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if valid_login(request.form['username'],
+                       request.form['password']):
+            return log_the_user_in(request.form['username'])
+        else:
+            error = 'Invalid username/password'
+    # the code below is executed if the request method
+    # was GET or the credentials were invalid
+    return render_template('login.html', error=error)
+
+
+
+#LOGOUT PAGE
+@app.route('/logout')
+def logout():
+    logout_user()
+    return render_template('logout.html') #ou : return redirect(url.for('index'))
+
+
+
+
+
+#mettre le truc quand connexion =sécurité
+

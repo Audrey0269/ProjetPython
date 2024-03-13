b7fb061f8c6867b4295ca9fe1bd17465e6df7570
GAUDILL1 <GAUDILL1@FRLAPP143>
2024-03-13 11:43:23 +0100

    add registration route, post and html

--- script.py
+++ script.py
@@ -1,49 +1,66 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
 from flask import url_for
 from markupsafe import escape
 from flask import request
 
-
 app = Flask(__name__)
 
 
-
-#HOME PAGE
-@app.route('/')
-def home():
-    return render_template('home.html')
-
-#RECIPE PAGE
-@app.route('/recipes') #/<int:id>')
-def recipes(): #mettre (id) entre les parenthèses
-    return render_template('recipes.html') #mettre apres : html', id=id)
-
-
 #LOGIN PAGE
-@app.route('/login', methods=['POST', 'GET'])
+@app.route('/')
 def login():
-    error = None
-    if request.method == 'POST':
-        if valid_login(request.form['username'],
-                       request.form['password']):
-            return log_the_user_in(request.form['username'])
-        else:
-            error = 'Invalid username/password'
-    # the code below is executed if the request method
-    # was GET or the credentials were invalid
-    return render_template('login.html', error=error)
+        return render_template('home.html')
+
+@app.route('/', methods=['POST'])
+def login_post():
+    username = "username" #request.form.get('username')
+    password = "password" #request.form.get('password')
+    return redirect(url_for('recipes'))
+
+#REGISTRATION PAGE
+@app.route('/registration')
+def registration():
+        return render_template('registration.html')
+
+@app.route('/registration', methods=['POST'])
+def registration_post():
+    username = "username" #request.form.get('username')
+    password = "password" #request.form.get('password')
+    email = "email" #request.form.get('email')
+    return redirect(url_for('login'))
+
+
+
+
+
+ 
+#RECIPES PAGE
+@app.route('/cooking_recipes')
+def recipes():
+    return render_template('recipes.html')
+
+
+#CREATE RECIPE PAGE
+@app.route('/create_cooking_recipes')
+def recipes():
+    return render_template('recipes.html')
 
 
 
 #LOGOUT PAGE
 @app.route('/logout')
 def logout():
-    logout_user()
-    return render_template('logout.html') #ou : return redirect(url.for('index'))
+    return redirect(url_for('login'))
 
 
 
 
+
+
+#RECIPE PAGE
+# @app.route('/recipe') #/<int:id>')
+# def recipes(): #mettre (id) entre les parenthèses
+#     return render_template('recipes.html') #mettre apres : html', id=id)
 
 #mettre le truc quand connexion =sécurité
 

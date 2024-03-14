8e11b30a03d210c0a56e6fba75eeeca9ba11e887
GAUDILL1 <GAUDILL1@FRLAPP143>
2024-03-14 12:12:51 +0100

    update folders database

---
+++ schema.sql
@@ -0,0 +1,35 @@
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS recipes;
+
+
+-- __________CREATE____________
+CREATE TABLE users(
+    id INT PRIMARY KEY AUTOINCREMENT,
+    username TEXT NOT NULL,
+    password TEXT NOT NULL,
+    email TEXT UNIQUE
+);
+
+CREATE TABLE recipes(
+    id INT PRIMARY KEY AUTOINCREMENT,
+    name TEXT NOT NULL,
+    description TEXT NOT NULL,
+    image TEXT
+);
+
+
+-- __________INSERT____________
+INSERT INTO users (id, username, password, email)
+ VALUES (1, 'Audrey', '123456', 'a.gaudilliere@gmail.com')
+
+ INSERT INTO recipes (id, name, description, image)
+ VALUES (1, 'Tiramisu', 'Dessert italien', 'tiramisu.jpg')
+        
+
+
+
+
+
+
+
+
--- script.py
+++ script.py
@@ -1,40 +1,72 @@
 from flask import Flask, render_template, redirect, url_for, request
 from markupsafe import escape
+from flask import g
+
+import sqlite3
 
 app = Flask(__name__)
 
 
+#______________________________DATABASE______________________________
+DATABASE = "database.db"
+
+# CONNEXION TO DATABASE (create empty database)
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = sqlite3.connect(DATABASE)
+        db.row_factory = sqlite3.Row
+    return db
+
+# CLOSE DATABASE
+@app.teardown_appcontext
+def close_connection(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
+
+#_______________________________APP___________________________
+
 #HOME PAGE
 @app.route('/')
 def home():
+    #Create cursur to SQL
+    cur = get_db().cursor()
+    cur.execute('SELECT * FROM recipes')
+
+
+
+
+    # for user in query_db('select * from users'):
+    # print(user['username'], 'has the id', user['user_id'])
+
+     # rv = cur.fetchall()
+    # cur.close()
+
     return render_template('home.html')
 
-# @app.route('/cookingRecipes', methods=['GET'])
-# def cooking_recipes_get():
-#     name = "name" #request.form.get('name')
-#     description = "descrciption" #request.form.get('description')
-#     image = "image" #request.form.get('image')
-#     return render_template('recipes.html')
 
+
+#SELECT*FROM TABLE
 
 #LOGIN PAGE
 @app.route('/login')
 def login():
     return render_template('login.html')
 
-# @app.route('/login', methods=['POST', 'GET'])
-# def login_post():
+@app.route('/login', methods=['POST', 'GET'])
+def login_post():
 
 #     #Check if user exist in database
 #     #user = User.query.filter_by(email=email).first()
 #     #if not user:
 #         #flash('Please ...')
+    username = request.form.get('username')
+    password = request.form.get('password')
+    return redirect(url_for('home'))#, {escape(username)}
 
-#     #Retrive username and password from form
-#     username = request.form.get('username')
-#     password = request.form.get('password')
-#     return redirect(url_for('recipes'))#, {escape(username)}
-
+#pour page de connexion faire : requete SELECT quand user = et password = 
+#bien faire commit à chaque modification de la BDD 
 
 
 #REGISTRATION PAGE
@@ -42,24 +74,13 @@ def login():
 def registration():
     return render_template('registration.html')
 
-# @app.route('/registration', methods=['POST'])
-# def registration_post():
-#     #Retrive values from form
-#     username = "username" #request.form.get('username')
-#     password = "password" #request.form.get('password')
-#     email = "email@email.fr" #request.form.get('email')
-#     return redirect(url_for('login'))#, {escape(username)}
-
-
-
-
-
-
-
- 
-
-
-
+@app.route('/registration', methods=['POST'])
+def registration_post():
+    #Retrive values from form
+    username = "username" #request.form.get('username')
+    password = "password" #request.form.get('password')
+    email = "email@email.fr" #request.form.get('email')
+    return redirect(url_for('login'))#, {escape(username)}
 
 
 #CREATE RECIPE PAGE
@@ -89,5 +110,13 @@ def logout():
 
 
 
+#__________________________DATABASE__________________________
 
+# IF DATABASE NOT EXIST
+if DATABASE is None:
+    with app.app_context():
+        db = get_db()
+        with app.open_resource('schema.sql', mode='r') as f:
+            db.cursor().executescript(f.read())
+        db.commit()
 
